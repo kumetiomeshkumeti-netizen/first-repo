ed466be7319e8d01168fe5854917542c4c29b870
omeshwar kumeti <kumetiomeshkumeti@gmail.com>
2025-10-06 09:56:05 +0000

    conversion

---
+++ .devcontainer/basics/conversion.js
@@ -0,0 +1,23 @@
+let score=33
+let score1="33"
+console.log(typeof(score))//
+console.log(typeof(score1))//string
+
+//type conversion
+let valueInNumber=Number(score1)
+console.log(typeof(valueInNumber))
+
+let isLoggedIn=1
+console.log(isLoggedIn)
+console.log(typeof(isLoggedIn))
+//1=>true
+//0=>false
+//""=>false
+//"hitesh"=>true
+//"0"=>true
+//null=>false
+//undefined=>false
+let somenumber=33
+let stringNumber=String(somenumber)
+console.log (stringNumber)
+console.log(typeof(stringNumber))
